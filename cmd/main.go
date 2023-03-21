c4f2bb686e0356daf22a53bbd6a81abd7161a96b
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-03-21 09:36:33 -0300

    feat: main project

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"go-sse/pkg/rabbitmq"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+
+	out := make(chan amqp.Delivery)
+
+	rabbitmqChann, err := rabbitmq.OpenChannel()
+
+	if err != nil {
+		panic(err)
+	}
+
+	go rabbitmq.Consume("msgs", rabbitmqChann, out)
+
+	http.HandleFunc("/sse", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		for m := range out {
+			fmt.Fprint(w, "event: message\n")
+			fmt.Fprintf(w, "data: %s\n\n", m.Body)
+			w.(http.Flusher).Flush()
+		}
+
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		http.ServeFile(w, r, "html/index.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
